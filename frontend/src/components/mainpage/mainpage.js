ff1ed5833c1999ae024d7bfa73c712494622f9b8
Naomi <naomi.tesfay@gmail.com>
2018-11-20 15:35:45 +0100

    added more moods

--- frontend/src/components/mainpage/mainpage.js
+++ frontend/src/components/mainpage/mainpage.js
@@ -8,21 +8,20 @@ const emotionData = [
 {moodType: "calm", points: 10},
 {moodType: "social", points: 10},
 {moodType: "active", points: 10},
-{moodType: "rested", points: 10}
-{moodType: "alert", points: 10}
-{moodType: "focused", points: 10}
-{moodType: "fearless", points: 10}
-{moodType: "optimistic", points: 10}
+{moodType: "rested", points: 10},
+{moodType: "alert", points: 10},
+{moodType: "focused", points: 10},
+{moodType: "fearless", points: 10},
+{moodType: "optimistic", points: 10},
 {moodType: "sad", points: 5},
-{moodType: "cheerful", points: 5},
 {moodType: "stressed", points: 5},
 {moodType: "angry", points: 5},
-{moodType: "numb", points: 5}
-{moodType: "heavy", points: 5}
-{moodType: "unsocial", points: 5}
-{moodType: "fatigue", points: 5}
-{moodType: "pessimistic", points: 5}
-{moodType: "unconcerned", points: 5}
+{moodType: "numb", points: 5},
+{moodType: "heavy", points: 5},
+{moodType: "unsocial", points: 5},
+{moodType: "fatigue", points: 5},
+{moodType: "pessimistic", points: 5},
+{moodType: "unconcerned", points: 5},
 {moodType: "irritated", points: 5}
 ]
 
@@ -66,20 +65,30 @@ class MainPage extends React.Component {
         <div className="mood-button">
         <button onClick={() => this.handleClick("happy")}>HAPPY</button>
         <button onClick={() => this.handleClick("cheerful")}>CHEERFUL</button>
+        <button onClick={() => this.handleClick("rested")}>RESTED</button>
+        <button onClick={() => this.handleClick("alert")}>ALERT</button>
         </div>
         <div className="mood-button">
         <button onClick={() => this.handleClick("calm")}>CALM</button>
         <button onClick={() => this.handleClick("social")}>SOCIAL</button>
+        <button onClick={() => this.handleClick("focused")}>FOCUSED</button>
+        <button onClick={() => this.handleClick("fearless")}>FEARLESS</button>
         </div>
         <div className="mood-button">
+        <button onClick={() => this.handleClick("optimistic")}>OPTIMISTIC</button>
         <button onClick={() => this.handleClick("active")}>ACTIVE</button>
         <button onClick={() => this.handleClick("sad")}>SAD</button>
+        <button onClick={() => this.handleClick("heavy")}>HEAVY</button>
         </div>
         <div className="mood-button">
         <button onClick={() => this.handleClick("stressed")}>STRESSED</button>
         <button onClick={() => this.handleClick("unsocial")}>UNSOCIAL</button>
+        <button onClick={() => this.handleClick("fatigue")}>FATIGUE</button>
+        <button onClick={() => this.handleClick("pessimistic")}>PESSIMISTIC</button>
         </div>
         <div className="mood-button">
+        <button onClick={() => this.handleClick("unconcerned")}>UNCONCERNED</button>
+        <button onClick={() => this.handleClick("IRRITATED")}>IRRITATED</button>
         <button onClick={() => this.handleClick("angry")}>ANGRY</button>
         <button onClick={() => this.handleClick("numb")}>NUMB</button>
         </div>
